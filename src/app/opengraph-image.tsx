d62a19b0cc0bbfd39aa54bec4f992bb3f2b6f7eb
Ahmad Faizi Kamil <jadiumrah.jakarta@gmail.com>
2026-04-20 15:36:22 +0700

    fix layout1

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,6 +2,13 @@ import { generateOG } from "@/lib/og/template";
 
 export const runtime = "edge";
 
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
 export default function Image() {
   return generateOG({
     title: "Ditelaga Creative Digital",
